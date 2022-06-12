2824d376f81a26ff7bf0e063c0a5b15a14bf8227
Anton Shapovalov <asshapovalov@delimobil.ru>
2022-06-12 21:04:29 +0300

    restrict move area by container

--- src/UI/drag-n-drop/useDrag.tsx
+++ src/UI/drag-n-drop/useDrag.tsx
@@ -2,15 +2,38 @@ import { MutableRefObject, useEffect } from 'react';
 
 import { useDragContainers } from '@/UI/drag-n-drop/useDragContainer';
 
+const needToStopMove = (element: HTMLElement, parent: HTMLElement) => {
+  const {
+    top: elementTop,
+    right: elementRight,
+    bottom: elementBottom,
+    left: elementLeft,
+  } = element.getBoundingClientRect();
+  const {
+    top: parentTop,
+    right: parentRight,
+    bottom: parentBottom,
+    left: parentLeft,
+  } = parent.getBoundingClientRect();
+
+  return {
+    top: parentTop - elementTop,
+    right: parentRight - elementRight,
+    bottom: parentBottom - elementBottom,
+    left: parentLeft - elementLeft,
+  };
+};
+
 export function useDrag(
   dragContainerName: string,
   dragItem: MutableRefObject<HTMLElement | null>,
 ) {
+  const { containers } = useDragContainers();
   const itemCoords = {
     x: 0,
     y: 0,
   };
-  const { containers } = useDragContainers();
+
   const onMouseUp = (evt: MouseEvent) => {
     evt.preventDefault();
     containers[dragContainerName].removeEventListener('mousemove', onMouseMove);
@@ -24,10 +47,16 @@ export function useDrag(
       console.warn('Cant move drag item');
       return;
     }
+    const availableDelata = needToStopMove(
+      dragItem.current,
+      containers[dragContainerName],
+    );
 
-    itemCoords.x += evt.movementX;
+    if (evt.movementX < 0 && evt.movementX > availableDelata.left) {
+      itemCoords.x += evt.movementX;
+    }
+
     itemCoords.y += evt.movementY;
-
     dragItem.current.style.transform = `translateX(${itemCoords.x}px) translateY(${itemCoords.y}px)`;
   };
 
